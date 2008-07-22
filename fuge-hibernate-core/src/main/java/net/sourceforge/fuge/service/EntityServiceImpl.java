742fd1650ac1fd87aeb9d3764a8cf5928a63c0de
allysonlister <allysonlister@eab3323b-7544-0410-af16-abf8cc3b93a6>
2008-07-22 08:23:27 +0000

    Incorrect copyright statement in EntityServiceImpl.java, and incorrect directory name in a comment of pom.xml

--- fuge-hibernate-core/src/main/java/net/sourceforge/fuge/service/EntityServiceImpl.java
+++ fuge-hibernate-core/src/main/java/net/sourceforge/fuge/service/EntityServiceImpl.java
@@ -1,19 +1,32 @@
 /**
  * Copyright Notice
  *
- *    Â© 2007-8 Proteomics Standards Initiative / Microarray and Gene Expression
- *    Data Society
+ * The MIT License
  *
- *    This is a document distributed under the terms of the
- *    Creative Commons Attribution License
- *    (http://creativecommons.org/licenses/by/3.0/), which permits
- *    unrestricted use, distribution, and reproduction in any medium,
- *    provided the original work is properly cited.
+ * Copyright (c) 2008 2007-8 Proteomics Standards Initiative / Microarray and Gene Expression Data Society
  *
- *    Acknowledgements
- *    The authors wish to thank the Proteomics Standards Initiative for
- *    the provision of infrastructure and expertise in the form of the PSI
- *    Document Process that has been used to formalise this document.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Acknowledgements
+ *  The authors wish to thank the Proteomics Standards Initiative for
+ *  the provision of infrastructure and expertise in the form of the PSI
+ *  Document Process that has been used to formalise this document.
  */
 /**
  * This is only generated once! It will never be overwritten.
